481a0ff47979650838be2b98c83cafd9b3c66a7d
Ash <relativistic.policeman@gmail.com>
2018-11-30 18:11:57 -0800

    Dumb program that just draws "Now Playing" until you kill it hard.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(nll)]
+use failure;
+use std::io;
+use termion::raw::IntoRawMode;
+use tui;
+use tui::layout::Rect;
+use tui::widgets;
+use tui::widgets::Widget;
+
+struct App {
+    size: Rect,
+}
+
+fn main() -> Result<(), failure::Error> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let backend = tui::backend::TermionBackend::new(stdout);
+    let mut terminal = tui::Terminal::new(backend)?;
+    let app = App {
+        size: terminal.size()?,
+    };
+    println!("{}", termion::clear::All);
+    loop {
+        terminal.draw(|mut f| {
+            widgets::Block::default()
+                .title("Now Playing")
+                .borders(widgets::Borders::ALL)
+                .render(&mut f, app.size);
+        })?;
+    }
+    Ok(())
 }
