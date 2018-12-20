879920a37d4524611eda828fdd33497d673bb53b
Ash <relativistic.policeman@gmail.com>
2018-12-19 21:59:20 -0800

    Draw the timestamp in the NowPlaying widget.

--- src/widgets/now_playing.rs
+++ src/widgets/now_playing.rs
@@ -28,6 +28,18 @@ impl NowPlaying {
 impl Widget for NowPlaying {
     fn draw(&mut self, area: tui::layout::Rect, buf: &mut tui::buffer::Buffer) {
         if let Some(ref song) = self.song {
+            if let Some(ref elapsed) = self.elapsed {
+                let seconds = elapsed.num_seconds() - elapsed.num_minutes() * 60;
+                let text = format!("{}:{:02}", elapsed.num_minutes(), seconds);
+                let len = text.len();
+                buf.set_stringn(
+                    area.left() + area.width - (len as u16),
+                    area.top(),
+                    text,
+                    len,
+                    tui::style::Style::default(),
+                )
+            }
             let title = song.title.as_ref().unwrap_or(&"Unknown".into()).clone();
             let artist = song.tags.get("Artist").unwrap_or(&"Unknown".into()).clone();
             let album = song.tags.get("Album").unwrap_or(&"Unknown".into()).clone();
