c2a830914256a6915502b3a1eca9a535fe6485ef
Ash <relativistic.policeman@gmail.com>
2018-12-27 23:02:31 -0600

    An operation for combining styles.

--- mimi/src/style.rs
+++ mimi/src/style.rs
@@ -40,3 +40,52 @@ impl Default for Style {
         }
     }
 }
+
+impl Style {
+    /// Combines the two styles. If `other` has a foreground color specified,
+    /// the combined style uses that color; else, `self`'s color is used. The
+    /// same logic applies to background colors. The modifier sets are unioned.
+    ///
+    /// This operation is associative (i.e., `foo.combine(bar.combine(baz)) ==
+    /// (foo.combine(bar)).combine(baz)` but not commutative (i.e.,
+    /// `foo.combine(bar) != bar.combine(foo)`), and
+    /// `foo.combine(Style::default()) == Style::default().combine(foo) == foo`.
+    /// If you're a math nerd, `Style::combine` forms a monoid.
+    pub fn combine(&self, other: &Style) -> Style {
+        Style {
+            foreground: other.foreground.or(self.foreground),
+            background: other.background.or(self.background),
+            modifiers: &other.modifiers | &self.modifiers,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn color_override() {
+        let red_foreground = Style {
+            foreground: Some(Color::Red),
+            ..Style::default()
+        };
+        let green_background = Style {
+            background: Some(Color::Green),
+            ..Style::default()
+        };
+
+        assert_eq!(
+            red_foreground.combine(&green_background),
+            Style {
+                foreground: Some(Color::Red),
+                background: Some(Color::Green),
+                ..Style::default()
+            }
+        );
+        assert_eq!(
+            red_foreground.combine(&green_background),
+            green_background.combine(&red_foreground)
+        )
+    }
+}
