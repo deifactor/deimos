f18988e01c0ffcd996fb7d85db77206e9b4ab244
Ash <ext0l@catgirl.ai>
2023-07-05 18:06:17 -0700

    fix: actually use track numbers

--- deimos/src/library.rs
+++ deimos/src/library.rs
@@ -1,10 +1,8 @@
-use anyhow::{anyhow, Result};
-use sqlx::{sqlite::SqliteConnectOptions, Executor, Pool, Sqlite, SqlitePool, Transaction};
+use anyhow::{Context, Result};
+use lofty::{Accessor, TaggedFileExt};
+use sqlx::{sqlite::SqliteConnectOptions, Pool, Sqlite, SqlitePool, Transaction};
 use std::{fs::File, os::unix::prelude::OsStrExt, path::Path};
-use symphonia::core::{
-    io::MediaSourceStream,
-    meta::{Metadata, StandardTagKey, Value},
-};
+use symphonia::core::io::MediaSourceStream;
 
 use walkdir::WalkDir;
 
@@ -36,49 +34,26 @@ pub async fn find_music(path: impl AsRef<Path>, conn: &mut Transaction<'_, Sqlit
             &Default::default(),
             &Default::default(),
         );
-        let Ok(mut probed) = probed else { continue; };
-        if let Some(metadata) = probed.metadata.get() {
-            insert_song(entry.path(), &metadata, conn).await?;
+        if probed.is_err() {
+            continue;
         }
+        let _ = insert_song(entry.path(), conn).await;
     }
     Ok(())
 }
 
-async fn insert_song(
-    path: &Path,
-    metadata: &Metadata<'_>,
-    conn: &mut Transaction<'_, Sqlite>,
-) -> Result<()> {
-    let mut title: Option<String> = None;
-    let mut album: Option<String> = None;
-    let mut artist: Option<String> = None;
-    let mut number: Option<i64> = None;
-    for tag in metadata
-        .current()
-        .ok_or(anyhow!("no metadata found"))?
-        .tags()
-    {
-        use StandardTagKey::*;
-        match (tag.std_key, &tag.value) {
-            (Some(TrackTitle), Value::String(s)) => title = Some(s.clone()),
-            (Some(Album), Value::String(s)) => album = Some(s.clone()),
-            (Some(AlbumArtist), Value::String(s)) => artist = Some(s.clone()),
-            (Some(TrackNumber), Value::UnsignedInt(i)) => number = Some(*i as i64),
-            (Some(TrackNumber), Value::SignedInt(i)) => number = Some(*i),
-            (Some(Artist), Value::String(s)) => {
-                if artist.is_none() {
-                    artist = Some(s.clone())
-                }
-            }
-            _ => (),
-        }
-    }
-
+async fn insert_song(path: &Path, conn: &mut Transaction<'_, Sqlite>) -> Result<()> {
+    let tagged_file = lofty::read_from_path(path)?;
+    let tag = tagged_file.primary_tag().context("no tags found")?.clone();
     let path_bytes = path.as_os_str().as_bytes();
+    let track = tag.track();
+    let title = tag.title();
+    let album = tag.album();
+    let artist = tag.artist();
     sqlx::query!(
         "INSERT INTO songs (path, number, title, album, artist) VALUES (?, ?, ?, ?, ?)",
         path_bytes,
-        number,
+        track,
         title,
         album,
         artist
