e94225c74d13d3da67982c215a2eab2c4aab9554
Ash <ext0l@catgirl.ai>
2023-11-01 23:21:30 -0700

    refactor: always use the same cpal stream
    The new design means we change what track is playing by changing out the
    value pointed to by an `Arc<Mutex<Option<Source>>>`.

--- deimos/src/audio/mod.rs
+++ deimos/src/audio/mod.rs
@@ -1,4 +1,5 @@
 use std::{
+    iter,
     sync::{Arc, Mutex},
     time::Duration,
 };
@@ -6,7 +7,7 @@ use std::{
 use anyhow::{Context, Result};
 use cpal::{
     traits::{DeviceTrait, HostTrait},
-    Device, Stream,
+    Stream,
 };
 use itertools::Itertools;
 use symphonia::core::audio::{AudioBuffer, SampleBuffer};
@@ -19,11 +20,12 @@ use self::reader::{Fragment, SymphoniaReader};
 mod reader;
 
 pub struct Player {
-    reader: Option<Arc<Mutex<SymphoniaReader>>>,
+    /// Provides an iterator over indiviudal samples as well as access to the underlying reader.
+    source: Arc<Mutex<Option<Source>>>,
     tx_message: UnboundedSender<Message>,
-    stream: Option<Stream>,
-
-    device: Device,
+    /// Streams audio to the underlying OS audio library. We set this up on construction and never
+    /// change it; instead, we just modify what `source` points to.
+    _stream: Stream,
 }
 
 pub enum PlayerMessage {
@@ -40,18 +42,32 @@ impl Player {
         let device = host
             .default_output_device()
             .context("no default output device")?;
+        let source: Arc<Mutex<Option<Source>>> = Arc::new(Mutex::new(None));
+        let config = device.default_output_config()?.config();
+        let source_clone = Arc::clone(&source);
+        let stream = device.build_output_stream(
+            &config,
+            move |data: &mut [f32], _| {
+                source_clone.lock().unwrap().as_mut().map(|iter| {
+                    for (dst, src) in data.iter_mut().zip(iter.by_ref()) {
+                        *dst = src
+                    }
+                });
+            },
+            |e| {
+                dbg!(e);
+            },
+            None,
+        )?;
         Ok(Self {
-            reader: None,
+            source,
             tx_message,
-            stream: None,
-            device,
+            _stream: stream,
         })
     }
 
     pub fn play_track(&mut self, track: Arc<Track>) -> Result<()> {
         let reader = Arc::new(Mutex::new(SymphoniaReader::from_path(&track.path)?));
-        self.reader = Some(Arc::clone(&reader));
-
         let tx_message = self.tx_message.clone();
         let on_decode: DecodeCallback = Box::new(move |fragment| {
             let _ = tx_message.send(Message::Player(PlayerMessage::AudioFragment {
@@ -61,85 +77,69 @@ impl Player {
             }));
         });
         let on_finish: FinishCallback = Box::new(|| ());
-
-        // get an iterator over the raw samples
         let source = Source::new(reader, on_decode, on_finish);
-        let mut sample_iter = source
-            .flat_map(|samples| samples.samples().iter().copied().collect_vec())
-            .fuse();
-
-        let config = self.device.default_output_config()?.config();
-        let stream = self.device.build_output_stream(
-            &config,
-            move |data: &mut [f32], _| {
-                for (dst, src) in data.iter_mut().zip(sample_iter.by_ref()) {
-                    *dst = src
-                }
-            },
-            |e| {
-                dbg!(e);
-            },
-            None,
-        )?;
-        self.stream = Some(stream);
+        *self.source.lock().unwrap() = Some(source);
         Ok(())
     }
 
     /// Seek to the given timestamp. Does nothing if there's no currently-playing track.
     pub fn seek(&mut self, target: Duration) -> Result<()> {
-        let Some(reader) = self.reader.as_mut() else {
-            return Ok(());
-        };
-        let mut reader = reader.lock().unwrap();
-        reader.seek(target)
+        let mut source = self.source.lock().unwrap();
+        if let Some(source) = source.as_mut() {
+            source.reader.lock().unwrap().seek(target)
+        } else {
+            Ok(())
+        }
     }
 }
 
 type DecodeCallback = Box<dyn FnMut(Fragment) + Send + 'static>;
 type FinishCallback = Box<dyn FnOnce() + Send + 'static>;
 
-/// Wraps a [`SymphoniaReader`] in order to invoke callbacks. This takes out a lock on the
-/// reader when it needs to decode a new buffer.
+/// Iterates over the samples of a reader, invoking callbacks on decode and on finish. Also
+/// provides access to the underlying reader so you can seek on it.
 struct Source {
     reader: Arc<Mutex<SymphoniaReader>>,
-    /// Invoked every time the inner reader decodes another sample buffer.
-    on_decode: DecodeCallback,
-    /// Invoked at most once when the inner reader has a (non-retryable) decode failure.
-    on_finish: Option<FinishCallback>,
+    iterator: Box<dyn Send + Iterator<Item = f32>>,
 }
 
 impl Source {
     fn new(
         reader: Arc<Mutex<SymphoniaReader>>,
-        on_decode: DecodeCallback,
+        mut on_decode: DecodeCallback,
         on_finish: FinishCallback,
     ) -> Self {
+        let reader_clone = Arc::clone(&reader);
+        let mut on_finish = Some(on_finish);
+        let iterator = iter::from_fn(move || {
+            let samples = reader_clone.lock().unwrap().next().map(|fragment| {
+                let buffer = &fragment.buffer;
+                let mut samples = SampleBuffer::new(buffer.capacity() as u64, *buffer.spec());
+                samples.copy_interleaved_typed(buffer);
+                (on_decode)(fragment);
+                samples
+            });
+            if samples.is_none() {
+                if let Some(f) = on_finish.take() {
+                    f()
+                }
+            }
+            samples
+        })
+        .flat_map(|samples| samples.samples().iter().copied().collect_vec())
+        .fuse();
+
         Self {
             reader,
-            on_decode,
-            on_finish: Some(on_finish),
+            iterator: Box::new(iterator),
         }
     }
 }
 
 impl Iterator for Source {
-    type Item = SampleBuffer<f32>;
+    type Item = f32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.reader.lock().unwrap().next() {
-            Some(fragment) => {
-                let buffer = &fragment.buffer;
-                let mut samples = SampleBuffer::new(buffer.capacity() as u64, *buffer.spec());
-                samples.copy_interleaved_typed(buffer);
-                (self.on_decode)(fragment);
-                Some(samples)
-            }
-            None => {
-                if let Some(f) = self.on_finish.take() {
-                    f()
-                }
-                None
-            }
-        }
+        self.iterator.next()
     }
 }
